d15aaba49901a863c13c9dd097e6a2eea771dc8f
adriana <picadoadriana7@gmail.com>
2020-11-15 17:36:30 -0500

    commit tree 7bd1f8ca9e8a39d79f961cc94018f980543478ac

--- spec/banking_spec.rb
+++ spec/banking_spec.rb
@@ -39,16 +39,16 @@ describe "BankAccount" do
   end
 
   describe '#valid?' do
-    it "is valid with an open status and a balance greater than 0" do
-      @broke = BankAccount.new("Kat Dennings")
-      @broke.balance = 0
-      @closed = BankAccount.new("Beth Behrs")
-      @closed.status = "closed"
-      expect(avi.valid?).to eq(true)
-      expect(@broke.valid?).to eq(false)
-      expect(@closed.valid?).to eq(false)
-    end
+  it "is valid with an open status and a balance greater than 0" do
+    @broke = BankAccount.new("Kat Dennings")
+    @broke.balance = 0
+    @closed = BankAccount.new("Beth Behrs")
+    @closed.status = "closed"
+    expect(avi.valid?).to eq(true)
+    expect(@broke.valid?).to eq(false)
+    expect(@closed.valid?).to eq(false)
   end
+end
 
   describe '#close_account' do
     it "can close its account" do
